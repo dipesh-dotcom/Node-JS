25fafa8b53779df3fb41684cd339825a6fe36f4a
Dipesh Shrestha <dipeshsth00@gmail.com>
2025-08-19 15:02:29 +0545

    working with module

---
+++ 02_module/main.js
@@ -0,0 +1,6 @@
+const math = require("./math");
+// const { add, sub } = require("./math");
+
+//console.log("Tha valus of math is : ", math.add(3, 2));
+
+console.log("Tha valus of math is : ", math.add(3, 2));
---
+++ 02_module/math.js
@@ -0,0 +1,10 @@
+// const add = (a, b) => a + b;
+// const sub = (a, b) => a - b;
+
+// module.exports = {
+//   add,
+//   sub,
+// };
+
+exports.add = (a, b) => a + b;
+exports.sub = (a, b) => a - b;
