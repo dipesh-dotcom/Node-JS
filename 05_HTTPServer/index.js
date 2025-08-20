fe4e19b8ca7c508656ddfaf0e36dad186a51d2be
Dipesh Shrestha <dipeshsth00@gmail.com>
2025-08-20 14:10:41 +0545

    creating a server

---
+++ 05_HTTPServer/index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+
+const myServer = http.createServer((req, res) => {
+  const log = `${Date.now()}: ${req.url} "NEw Req added\n`;
+
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Home page");
+        break;
+      case "/about":
+        res.end("I am Dipesh Shrestha");
+        break;
+      default:
+        res.end("404 Not Found");
+    }
+  });
+});
+
+myServer.listen("8000", () => console.log("Server started"));
