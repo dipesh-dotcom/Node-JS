7369a84b76c81f2333162d1c937d7b4d2943b105
Dipesh Shrestha <dipeshsth00@gmail.com>
2025-08-21 18:28:18 +0545

    learn about middleware

---
+++ 08_Middleware/index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const fs = require("fs");
+const users = require("./MOCK_DATA.json");
+const app = express();
+const port = 8000;
+
+// middleware
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  fs.appendFile(
+    "./log.txt",
+    `\n${Date.now()}: ${req.ip}: ${req.method} : ${req.url}\n`,
+    (err, data) => {
+      next();
+    }
+  );
+});
+
+//Routes
+
+app.get("/users", (req, res) => {
+  const html = `
+      <ul>
+          ${users.map((user) => `<li>${user.first_name}</li>`).join("")}  
+      </ul>
+      `;
+  return res.send(html);
+});
+
+// REST API
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app
+  .route("/api/user/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    if (!user) return res.json({ status: "404 not found" });
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    const id = Number(req.params.id);
+    const body = req.body;
+    const userIndex = users.findIndex((user) => user.id === id);
+    if (userIndex === -1) return res.json({ status: "404 not found" });
+
+    users[userIndex] = { ...users[userIndex], ...body };
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if (err) {
+        return res.status(500).json({ status: "Error saving data" });
+      }
+      return res.json({ status: "User updated", user: users[userIndex] });
+    });
+  })
+  .delete((req, res) => {
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    users.splice(userIndex, 1);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      return res.json({ status: "200 ok" });
+    });
+  });
+
+app.post("/api/user", (req, res) => {
+  const body = req.body;
+  users.push({ id: users.length + 1, ...body });
+
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "pending" });
+  });
+});
+
+app.listen(port, console.log(`Server listen on port ${port}`));
